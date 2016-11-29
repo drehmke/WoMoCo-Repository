eb1e071a58e782b752bf189d929c804cac9a268f
Donna Ryan <donna@nerdrose.com>
2016-11-28 17:41:18 -0700

    Adding the GenericRepository

---
+++ src/WoMoCo/Interfaces/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace WoMoCo.Interfaces
+{
+    public interface IGenericRepository
+    {
+        void Add<T>(T entityToCreate) where T : class;
+        void Delete<T>(T entityToDelete) where T : class;
+        void Dispose();
+        IQueryable<T> Query<T>() where T : class;
+        void SaveChanges();
+        IQueryable<T> SqlQuery<T>(string sql, params object[] parameters) where T : class;
+        void Update<T>(T entityToUpdate) where T : class;
+    }
+}
