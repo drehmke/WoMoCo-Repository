7a4c125264ac6f80e28a9d39d4c2812fbd353a39
Donna Ryan <donna@nerdrose.com>
2017-01-05 11:28:30 -0700

    Adding pulldown to new message view

--- src/WoMoCo/wwwroot/ngApp/controllers/createMessageController.ts
+++ src/WoMoCo/wwwroot/ngApp/controllers/createMessageController.ts
@@ -2,6 +2,8 @@
 
     export class CreateMessageController {
         public MessageResource;
+        public getPullDownResource;
+        public pulldownUsers;
         public comm;
 
         public saveComm() {
@@ -12,12 +14,28 @@
 
         }
 
+        public GetUsersForPullDown() {
+            let tempResults = this.getPullDownResource.getUsersForPulldown();
+            console.log(tempResults);
+            return tempResults;
+        }
+
         constructor(private $resource: angular.resource.IResourceService,
             public $stateParams: ng.ui.IStateParamsService,
             private $state: ng.ui.IStateService,
-            private accountService: WoMoCo.Services.AccountService) {
+            private accountService: WoMoCo.Services.AccountService,
+            private MessageService: WoMoCo.Services.MessageService
+        ) {
             this.MessageResource = this.$resource('/api/comms');
+            this.getPullDownResource = $resource(`/api/users`, null, {
+                getUsersForPulldown: {
+                    method: `GET`,
+                    url: `/api/users/GetUsersForPulldown`,
+                    isArray: true
+                }
+            });
 
+            this.pulldownUsers = this.GetUsersForPullDown();
         }
 
     }
